787e2566d0b8eb85e17bc2ce2900b2c166dfb283
Suyog Naik <naiksuyogsudhir@gmail.com>
2023-10-18 23:37:54 +0530

    second commit

--- OTP_version.py
+++ OTP_version.py
@@ -1,34 +1,69 @@
-import smtplib
-from twilio.rest import Client
 import math
 import random
+import smtplib
+from twilio.rest import Client
+import sms
 
-account_sid = "AC74d3234c1b27da3f53a19340e214c05f"
-auth_token = "07124dff1c5ee5945fe0179b0adbaa5d"
 
-inputNO = '+14705162791'
-sendNo = '+917666143540'
-# send to sms
-client = Client(sms.account_sid, sms.auth_token)
-data = "0123456789"
-leng = len(data)
-otp = ""
+def Validate_MobileNo(MobileNo):
+    if len(MobileNo) != 10:
+        print("Please enter valid Mobile number!!")
+        MobileNo = input("Enter the Mobile number:")
+        Validate_MobileNo(MobileNo)
+    return MobileNo
 
-for i in range(6):
-    otp += data[math.floor(random.random()*leng)]
 
-message = client.messages.create(
-    body="Your 6 digit OTP is "+otp,
+def Validate_Email(Email):
+    if "@gmail.com" not in Email:
+        print("Please Enter Valid Email address!!")
+        Email = input("Enter the Email:")
+        Validate_Email(Email)
+    return Email
 
-    from_=sms.inputNO,
-    to=sms.sendNo
-)
 
-print(message.body)
+def Generate_OTP():
+    digits = "0123456789"
+    length = len(digits)
+    otp = ""
+
+    for i in range(6):
+        otp += digits[math.floor(random.random()*length)]
+
+    return otp
+
+
+def send_email(Checked_email, OTP):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()  # transfer layer security
+    server.login('naiksuyogsudhir@gmail.com', 'kajzazcwvvnbhgxg')
+    message = 'Your 6 digit OTP is '+str(OTP)
+    server.sendmail('naiksuyogsudhir@gmail.com',
+                    Checked_email, message)
+    server.quit()
+
+
+def send_OTP(Checked_MobileNo, OTP):
+    client = Client(sms.account_sid, sms.auth_token)
+    Message = client.messages.create(
+        body="Your 6 digit OTP is "+OTP,
+
+        from_=sms.inputNO,
+        to='+91'+str(Checked_MobileNo),
+    )
+    print(Message.body)
+
+
+MobileNo = input("Enter the Mobile number:")
+Checked_MobileNo = Validate_MobileNo(MobileNo)
+
+Email = input("Enter the Email:")
+Checked_email = Validate_Email(Email)
+
+# will Generate OTP
+OTP = Generate_OTP()
+
+# send OTP to Number
+send_OTP(Checked_MobileNo, OTP)
+
 # send to email
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()  # transfer layer security
-server.login('naiksuyogsudhir@gmail.com', 'kajzazcwvvnbhgxg')
-msg = 'Your 6 digit OTP is '+str(otp)
-server.sendmail('naiksuyogsudhir@gmail.com', 'naiksuyog2004@gmail.com', msg)
-server.quit()
+send_email(Checked_email, OTP)
